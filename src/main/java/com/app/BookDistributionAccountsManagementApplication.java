58bc44c5758abd9df08dc131b23368691cd61e16
prasad108 <prasad.dukale@atos.net>
2019-02-26 09:14:21 +0530

    added feature to shutdown the application

--- src/main/java/com/app/BookDistributionAccountsManagementApplication.java
+++ src/main/java/com/app/BookDistributionAccountsManagementApplication.java
@@ -2,10 +2,6 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BookDistributionAccountsManagementApplication {
@@ -13,14 +9,4 @@ public class BookDistributionAccountsManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookDistributionAccountsManagementApplication.class, args);
 	}
-	@Bean
-	   public WebMvcConfigurer corsConfigurer() {
-	      return new WebMvcConfigurerAdapter() {
-	         @Override
-	         public void addCorsMappings(CorsRegistry registry) {
-	            registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
-                .allowedHeaders("*");
-	         }
-	      };
-	   }
 }
