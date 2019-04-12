d59dc08d08d70ed9f1b71e27c3536c59b1f0cd40
prasad108 <prasad.dukale@atos.net>
2019-04-12 20:51:47 +0530

    added not found exception

--- src/main/java/com/app/Book/BookController.java
+++ src/main/java/com/app/Book/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
 
+import com.app.Exceptions.ResourceNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,7 +43,7 @@ public class BookController {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	@RolesAllowed("ROLE_ADMIN")
 	public Book getBook(@PathVariable Integer id) {
-		return bookService.find(id);
+		return bookService.find(id).orElseThrow(()->  new ResourceNotFound("Book Not Found"));
 	}
 	
 	@RolesAllowed("ROLE_ADMIN")
